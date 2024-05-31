93ef13145785e979e94c1ca04d8350adfa25d26e
omega-beta <140123838+omega-beta@users.noreply.github.com>
2024-05-30 21:42:14 -0400

    Okay try to work with this

--- cards.js
+++ cards.js
@@ -27,18 +27,19 @@ function display1() {
     if (tryNum == 1) {
         // alert(tryNum);
         linear1.classList.toggle("appear");
-        linear1.style.display = "block";
+        // linear1.style.display = "block";
     }
     if (tryNum == 2) {
         // alert(tryNum);
         linear2.classList.toggle("appear");
-        linear2.style.display = "block";
+        // linear2.style.display = "block";
     }
     if (tryNum == 3) {
         // alert(tryNum);
         linear3.classList.toggle("appear");
-        linear3.style.display = "block";
+        // linear3.style.display = "block";
     }
+    tryNum = Math.floor(Math.random()*3)+1;
 }
 /* document.addEventListner('DOMContentLoaded', function(event) {
     button1.addEventListener('mousedown', display1());
